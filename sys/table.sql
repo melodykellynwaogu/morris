1660963d852503d74fb16a7770fe7d30632d59b3
melodykellynwaogu <melodynwaogu224@gmail.com>
2026-08-19 05:22:57 +0200

    Add SQL queries to fetch movie titles and years within specified year ranges

---
+++ sys/table.sql
@@ -0,0 +1,16 @@
+SELECT title, year
+FROM movies
+WHERE year BETWEEN 2000 AND 2010
+/* Or if not that year, you can change it to */
+WHERE year NOT BETWEEN 2000 AND 2010
+
+
+/* to use id, SELECT the id */
+
+SELECT title, id 
+FROM movie
+WHERE id = 6 /* 6 representing the movies title id row */
+
+SELECT title, year 
+FROM movies
+WHERE year <= 2003
