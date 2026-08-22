15f7fe9297d5a4f12fed7441c25d3980e2b8e2e9
melodykellynwaogu <melodynwaogu224@gmail.com>
2026-08-22 15:49:38 +0200

    Add movies.sql and table.sql for movie and grocery database schemas

---
+++ movies.sql
@@ -0,0 +1,14 @@
+
+
+CREATE TABLE cyber (
+    id INTEGER PRIMARY KEY,
+    name TEXT, 
+    rating INTEGER 
+);
+
+INSERT INTO cyber (1, 'Tuesday', 5);
+INSERT INTO cyber (2, 'Thusday', 6);
+INSERT INTO cyber (3, 'Friday', 7);
+INSERT INTO cyber (4, 'Sunday', 10);
+
+SELECT * FROM cyber
